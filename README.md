9a7d85a1922fac6fe1afb9f98b211c663ba9f983
xinchen <xinchen.sakura@gmail.com>
2024-05-09 16:33:33 +0800

    update gpio

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # tinyriscv-modified
 
+## GPIO的引出
+原有的 Tinyriscv 中引出了2个 GPIO, 现在需要将其修改为16个:
+- 修改 [gpio.v](./rtl/perips/gpio.v) 中的信号宽度, 为后14个引脚添加写入逻辑
+- 在顶层模块 [tinyriscv_soc_top.v](./rtl/soc/tinyriscv_soc_top.v) 中修改 io_in 信号的宽度, 并将gpio模块的所有输入输出引出到 SoC 的 gpio 管脚处
+- 验证程序见 [main.c](./tests/example/gpio_16/main.c), 在 `sim/` 路径下运行 `python ./sim_new_nowave.py ../tests/example/gpio_16/gpio.bin inst.data` , 之后查看波形
+
+## Acknowledgement
+https://gitee.com/liangkangnan/tinyriscv
--- tests/example/gpio/main.c
+++ tests/example/gpio/main.c
@@ -8,25 +8,13 @@ int main()
 {
     GPIO_REG(GPIO_CTRL) |= 0x1;       // gpio0输出模式
     GPIO_REG(GPIO_CTRL) |= 0x1 << 3;  // gpio1输入模式
-    // GPIO_REG(GPIO_CTRL) |= 0x1 << 4;  // gpio2输入模式
 
-    // while (1) {
-    //     // 如果GPIO1输入高
-    //     if (GPIO_REG(GPIO_DATA) & 0x2)
-    //         GPIO_REG(GPIO_DATA) |= 0x1;  // GPIO0输出高
-    //     // 如果GPIO1输入低
-    //     else
-    //         GPIO_REG(GPIO_DATA) &= ~0x1; // GPIO0输出低
-    // }
-
-    GPIO_REG(GPIO_DATA) |= 0x11;  // GPIO0输出高
-
-    if (GPIO_REG(GPIO_DATA) == 0x11) {
-        set_test_pass();
-    }     
-    else {
-        set_test_fail();
+    while (1) {
+        // 如果GPIO1输入高
+        if (GPIO_REG(GPIO_DATA) & 0x2)
+            GPIO_REG(GPIO_DATA) |= 0x1;  // GPIO0输出高
+        // 如果GPIO1输入低
+        else
+            GPIO_REG(GPIO_DATA) &= ~0x1; // GPIO0输出低
     }
-
-    return 0;
 }
